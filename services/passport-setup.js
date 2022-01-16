db9b5f76b37c3e284556dbbe5aa855459397013f
Sumna Islam <sumanislam1416@gmail.com>
2022-01-17 00:07:25 +0600

    passport strategy setup

--- services/passport-setup.js
+++ services/passport-setup.js
@@ -1,8 +1,16 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+require('dotenv').config();
+
+const config = {
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+}
 
 passport.use(new GoogleStrategy({
   // GoogleStrategy option goes here
+  clientID: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRET,
 }), () => {
   // passport callback function
 })
